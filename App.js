1fc7cb3dc6a3c8e7ce89f89a33c3a0cc96339b83
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2023-11-06 17:22:05 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-// import components
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+} from 'react-native';
 import Button from './components/Button';
 import Row from './components/Row';
 
 export default function App() {
-
   // State variables 
   const [displayValue, setDisplayValue] = useState('0'); 
   const [operator, setOperator] = useState(null); 
@@ -21,6 +22,13 @@ export default function App() {
       setDisplayValue(displayValue + num); 
     } 
   }; 
+
+  // Function to handle clear button press 
+  const handleClear = () => { 
+    setDisplayValue('0'); 
+    setOperator(null); 
+    setFirstValue(''); 
+  };   
 
   // Function to handle operator inputs 
   const handleOperatorInput = (operator) => { 
@@ -41,59 +49,67 @@ export default function App() {
     } else if (operator === '*') { 
       setDisplayValue((num1 * num2).toString()); 
     } else if (operator === '/') { 
-      setDisplayValue((num1 / num2).toString()); 
+      setDisplayValue((num1 / num2).toFixed(3).toString()); 
     } 
-
     setOperator(null); 
     setFirstValue(''); 
   };   
 
-  // Function to handle clear button press 
-  const handleClear = () => { 
-    setDisplayValue('0'); 
-    setOperator(null); 
-    setFirstValue(''); 
-  };   
+  // handle change sign
+  const handlePosneg = () => {
+    setDisplayValue((displayValue * -1).toString());
+  };
 
+  // handle percentage
+  const handlePercentage = () => {
+    setDisplayValue((displayValue * 0.01).toString());
+  };
 
   return (
     <View style={styles.container}>
-      {/* Status bae here */}
-      <Text style={styles.value}>
-        {displayValue}
-      </Text>
+
+      <View>
+        <Text style={styles.value}>
+          {displayValue}
+        </Text>
+      </View>
+      
       {/* Do create componentRow */}
+
       <Row>
         <Button text="C" theme="secondary" onPress={handleClear} />
-        <Button text="+/-" theme="secondary" onPress={() => HandleTap("posneg")} />
-        <Button text="%" theme="secondary" onPress={() => HandleTap("percentage")} />
+        <Button text="+/-" theme="secondary" onPress={() => handlePosneg("+/-")} />
+        <Button text="%" theme="secondary" onPress={() => handlePercentage("%")} />
         <Button text="/" theme="accent" onPress={() => handleOperatorInput("/")} />
       </Row>
 
-      {/* Number */}
       <Row>
         <Button text="7" onPress={() => handleNumberInput(7)} />
         <Button text="8" onPress={() => handleNumberInput(8)} />
         <Button text="9" onPress={() => handleNumberInput(9)} />
         <Button text="*" theme="accent" onPress={() => handleOperatorInput("*")} />
       </Row>
+
       <Row>
         <Button text="4" onPress={() => handleNumberInput(4)} />
         <Button text="5" onPress={() => handleNumberInput(5)} />
         <Button text="6" onPress={() => handleNumberInput(6)} />
         <Button text="-" theme="accent" onPress={() => handleOperatorInput("-")} />
       </Row>
+
       <Row>
         <Button text="1" onPress={() => handleNumberInput(1)} />
         <Button text="2" onPress={() => handleNumberInput(2)} />
         <Button text="3" onPress={() => handleNumberInput(3)} />
         <Button text="+" theme="accent" onPress={() => handleOperatorInput("+")} />
       </Row>
+
       <Row>
         <Button text="0" onPress={() => handleNumberInput(0)} />
         <Button text="." onPress={() => handleNumberInput(".")} />
-        <Button text="=" theme="primary" onPress={() => handleEqual("=")} />
+        <Button text="=" theme="accent" onPress={() => handleEqual("=")} />
       </Row>
+
       <StatusBar style="light" />
     </View>
   );
@@ -113,4 +129,3 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
 });
-
